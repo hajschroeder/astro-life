6730a969d46ea20003ce2ba3e735da4076f308f1
Hans Schroeder <hajschroeder@gmail.com>
2022-03-12 13:23:35 -0800

    update first test to include second property in constructor

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -3,8 +3,8 @@ import Galactic from '../src/galacticage.js';
 describe('Galactic', () => {
 
   test('should create a new object with properties', () =>{
-    let testAge1 = new Galactic(100)
-    expect(testAge1.age).toEqual(100)
+    let testAge1 = new Galactic(100, 85)
+    expect(testAge1.age).toEqual(100, 85)
   }) 
 
   describe('Galactic#planetYears()', () =>{
