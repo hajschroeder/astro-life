4bad9f5b56097b8d6500293a2943453125dc683e
Hans Schroeder <hajschroeder@gmail.com>
2022-03-12 11:24:48 -0800

    add new test to planetYears function

--- __tests__/galacticage.tests.js
+++ __tests__/galacticage.tests.js
@@ -10,21 +10,26 @@ describe('Galactic', () => {
   describe('Galactic#planetYears()', () =>{
 
     test('should return a year relative to a planet', () =>{
-      let testAge2 = new Galactic();
+      let testAge2 = new Galactic(1);
       testAge2.planetYears();
       expect(testAge2.earth).toEqual(1);
       expect(testAge2.mercury).toEqual(0.24);
       expect(testAge2.venus).toEqual(0.62);
       expect(testAge2.mars).toEqual(1.88);
-      expect(testAge2.jupiter).toEqual(1.86);
+      expect(testAge2.jupiter).toEqual(11.86);
+    })
+
+    test('should fill an array with values of the galactic years', () =>{
+      const planetArray = [];
+      expect(planetArray).toEqual(expect.arrayContaining(planetArray));
     })
   })
 
   describe ('Galactic#lifeExpectancy()', () =>{
 
-    test('should calculate the user life expectancy', () =>{
+    test('should consider variables of life expectancy', () =>{
       let testAge3 = new Galactic();
-      testAge3.lifeExpectancy()
+      testAge3.lifeExpectancy();
       expect(testAge3.male).toEqual(80);
       expect(testAge3.female).toEqual(90);
       expect(testAge3.smoker).toEqual(75);
@@ -32,8 +37,6 @@ describe('Galactic', () => {
       expect(testAge3.badDiet).toEqual(82);
       expect(testAge3.exercise).toEqual(89);
       expect(testAge3.sedentary).toEqual(81);      
-    }
-
-    )
+    })
   })
 })
