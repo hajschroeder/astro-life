26a931e2bcf3b509a0409430bdce28f3c5f60b35
Hans Schroeder <hajschroeder@gmail.com>
2022-03-12 14:38:39 -0800

    function passing with current approach

--- src/galacticage.js
+++ src/galacticage.js
@@ -1,8 +1,8 @@
 //Business Logic For Age
 export default class Galactic{
-  constructor(age, life){
+  constructor(age, lifestyle){
     this.age = age;
-    this.life = life;
+    this.lifestyle = lifestyle;
   }
   
   planetYears() {
@@ -25,7 +25,14 @@ export default class Galactic{
   
   
   lifeExpectancy() {
+    this.healthyMale = "Healthy Male";
+    this.smokerMale = "Male Smoker";
+    this.healthyFemale = "Healthy Female";
+    this.smokerFemale = "Female Smoker";
 
+    if (this.lifestyle === this.healthyMale){
+      this.lifeLength = 85
+    }
     }
   }
 
@@ -38,22 +45,3 @@ console.log(thisAge);
 console.log(thisAge.planetYears());
 
 
-// lifeExpectancy() {
-//   let base = 85;
-//   this.male = base - 5;
-//   this.female = base + 5;
-//   this.smoker = base -10;
-//   this.exercise = base + 4;
-
-//   if (this.male === true){
-//     return this.male;
-//   }
-// }
-
-// test('should consider variables of life expectancy', () =>{
-//   let testAge3 = new Galactic();
-//   testAge3.lifeExpectancy();
-//   expect(testAge3.male).toEqual(80);
-//   expect(testAge3.female).toEqual(90);
-//   expect(testAge3.smoker).toEqual(75);
-//   expect(testAge3.exercise).toEqual(89);   
